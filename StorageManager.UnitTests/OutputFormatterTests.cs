149e7033bedb71b5a3755541f4a1462bb2a0253d
Neon <porterneon@gmail.com>
2020-05-19 20:09:46 +0200

    remove unused references

--- StorageManager.UnitTests/OutputFormatterTests.cs
+++ StorageManager.UnitTests/OutputFormatterTests.cs
@@ -1,9 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using StorageManager.Models;
 using StorageManager.Services;
-using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace StorageManager.UnitTests
